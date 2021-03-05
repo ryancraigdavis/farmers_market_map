9ba4607021e5390085825a07e69e24ccc5f3fc1d
Ryan Davis <ryancraigdavis@gmail.com>
2021-03-04 19:56:12 -0800

    try 5

--- src/server/server.js
+++ src/server/server.js
@@ -13,7 +13,7 @@ const createDummy = require('./mysql/dummy-data');
 // MySQL pool connections
 const pool = mysql.createPool({
   	connectionLimit  : 10,
-  	host  : process.env.DB_HOST,
+  	host  : 'http://66.42.74.237/',
   	user  : process.env.DB_USER,
   	password: process.env.DB_PASS,
   	database: process.env.DB_NAME
